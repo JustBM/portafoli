91d60f9abbfda46043413ffaa57c230292d00501
JustBM <bueno.jm92@gmail.com>
2022-06-01 14:04:46 +0200

    Pagina con plantilla Urku

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+// Rutas
+import { Routes } from '@angular/router';
+// Components
+import { AppComponent } from './app.component';
+import { PortafolioComponent } from './shared/pages/portafolio/portafolio.component';
+import { AboutComponent } from './shared/pages/about/about.component';
+import { ItemsComponent } from './shared/pages/items/items.component';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { PortafolioComponent } from './shared/pages/portafolio/portafolio.component';
+import { AboutComponent } from './shared/pages/about/about.component';
+import { ItemsComponent } from './shared/pages/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    AboutComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
