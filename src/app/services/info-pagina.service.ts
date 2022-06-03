1b2c182f058e511e6ce0c538999a4c661cb3c92f
JustBM <bueno.jm92@gmail.com>
2022-06-03 10:55:57 +0200

    Mitad clase 03/06

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,19 +10,30 @@ export class InfoPaginaService {
   info: InfoPagina = {}; 
   //info: any = {}; //No deberia usarse any
   cargada = false;
+  equipo: any = []; 
 
   constructor( private http: HttpClient) { 
+
+    this.cargarInfo ();
+    this.cargarEquipo ();
     // Prueba de donde aparece
     // console.log('Servicio de infoPagina listo')
     //Leer el archivo JSON
-    this.http.get('../../assets/data/data-pagina.json')
-      .subscribe( resp => {
+    
+  }
 
+  //Métodos
+  private cargarInfo(); //para leer archivo de JSON interno
+    this.http.get('../../assets/data/data-pagina.json')
+      subscribe( resp => {
         this.cargada = true;
         this.info = resp;
         console.log( resp );
-        // console.log( resp['github'] );
+    })
 
-      })
-  }
-}
+private cargarEquipo(); //Para leer archivo de JSON externo
+  this.http.get('https://portfolio-angular-html-ddb-default-rtdb.europe-west1.firebasedatabase.app/Equip.json')
+    subscribe( resp => {
+      this.info = resp;
+      console.log( resp );
+})
